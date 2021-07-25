81889e8f00ac4b1531ae4cbeba93eb8b8611f1f2
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-07-25 12:59:21 +0700

    unit and integration tests

--- src/main/java/com/kondratyev/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/kondratyev/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,6 +1,5 @@
 package com.kondratyev.spring5recipeapp.repositories;
 
-import com.kondratyev.spring5recipeapp.domain.Category;
 import com.kondratyev.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
---
+++ src/test/java/com/kondratyev/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.kondratyev.spring5recipeapp.services;
+
+import com.kondratyev.spring5recipeapp.domain.Recipe;
+import com.kondratyev.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
