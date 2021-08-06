85306491a7e0837fe837ab8ad274299eb5d86c0b
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-08-06 17:51:24 +0700

    enumeration

--- src/main/java/com/kondratyev/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/com/kondratyev/spring5recipeapp/controllers/IngredientController.java
@@ -9,7 +9,10 @@ import com.kondratyev.spring5recipeapp.services.UnitOfMeasureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -82,8 +85,7 @@ public class IngredientController {
         return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
     }
 
-    @DeleteMapping // possibly not correct
-    @RequestMapping("recipe/{recipeId}/ingredient/{id}/delete")
+    @GetMapping("recipe/{recipeId}/ingredient/{id}/delete")
     public String deleteById(@PathVariable String recipeId,
                              @PathVariable String id){
 
--- src/main/java/com/kondratyev/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/kondratyev/spring5recipeapp/controllers/RecipeController.java
@@ -5,7 +5,10 @@ import com.kondratyev.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -44,8 +47,7 @@ public class RecipeController {
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @DeleteMapping // possibly not correct
-    @RequestMapping("recipe/{id}/delete")
+    @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
 
         log.debug("Deleting id: " + id);
